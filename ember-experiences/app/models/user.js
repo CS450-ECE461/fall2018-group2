4dab0ad892746d3d1afdde171202deaf1f4e3d55
Mateusjose <mateus.s.o.jose@gmail.com>
2018-11-05 14:43:42 -0500

    deleted the password from the users model because gatekeeper will be responsable for password

--- ember-experiences/app/models/user.js
+++ ember-experiences/app/models/user.js
@@ -4,7 +4,6 @@ import { computed } from '@ember/object';
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  password: DS.attr('string'),
   role: DS.attr(),
 
   fullName: computed('firstName', 'lastName', function() {
