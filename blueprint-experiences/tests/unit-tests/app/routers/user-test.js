f9934aefb431316301f3cd10c2390e7adb4f7b40
Leo <oleoneto@gmail.com>
2018-11-05 15:31:15 -0500

    Implemented user model

--- blueprint-experiences/tests/unit-tests/app/routers/user-test.js
+++ blueprint-experiences/tests/unit-tests/app/routers/user-test.js
@@ -3,17 +3,48 @@ const { seed } = require ('@onehilltech/blueprint-mongodb');
 
 describe ('app | routers | user', function () {
 
-  // Test CREATE and READ  methods on /v1/users
+  // -- Test CREATE and READ  methods on /v1/users
   describe ('/v1/users/', function () {
-    describe ('POST', function () {});
-    describe ('GET ALL', function () {});
+
+    describe ('GET ALL', function () {
+
+      it ('should get all users', function () {
+        return request()
+          .get('/v1/users')
+          .expect(200);
+      });
+
+    });
+
+    describe ('POST', function () {
+
+      it ('should create a user profile', function () {
+        const user = {
+          firstName: "Leo",
+          lastName: "Neto",
+          role: "admin"
+        };
+
+        return request()
+          .post('/v1/users/')
+          .send({user})
+          .expect(200);
+      });
+
+    });
+
   });
 
-  // Test READ, UPDATE, and DELETE methods on a user with given id
+
+  // -- Test READ, UPDATE, and DELETE methods on a user with given id
   describe ('/v1/users/:userId', function () {
-    describe ('GET', function () {});
-    describe ('PUT', function () {});
-    describe ('DELETE', function () {});
+
+    // -- Implement GET test
+
+    // -- Implement PUT test
+
+    // -- Implement DELETE test
+
   });
 
 });
