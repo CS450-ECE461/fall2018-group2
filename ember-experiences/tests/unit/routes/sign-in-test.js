12f845824ed814a36fe97e6a06ad2885da76e5ca
Seth Fuller <semfulle@iupui.edu>
2018-11-17 14:35:57 -0500

    Added tests to sign-in and sign-up routes and deleted acceptance-test login

--- ember-experiences/tests/unit/routes/sign-in-test.js
+++ ember-experiences/tests/unit/routes/sign-in-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { currentURL, click, visit } from '@ember/test-helpers';
 
 module('Unit | Route | sign-in', function(hooks) {
   setupTest(hooks);
@@ -7,5 +8,11 @@ module('Unit | Route | sign-in', function(hooks) {
   test('it exists', function(assert) {
     let route = this.owner.lookup('route:sign-in');
     assert.ok(route);
+  });
+
+  test('it redirects to sign-up view when sign-up link is clicked', async function(assert) {
+    await visit('/sign-in');
+    await click('.sign-up-link');
+    assert.equal(currentURL(), '/sign-up', 'should redirect to sign-up view');
   });
 });
--- ember-experiences/tests/unit/routes/sign-up-test.js
+++ ember-experiences/tests/unit/routes/sign-up-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { currentURL, click, visit } from '@ember/test-helpers';
 
 module('Unit | Route | sign-up', function(hooks) {
   setupTest(hooks);
@@ -7,5 +8,11 @@ module('Unit | Route | sign-up', function(hooks) {
   test('it exists', function(assert) {
     let route = this.owner.lookup('route:sign-up');
     assert.ok(route);
+  });
+
+  test('it redirects to sign-in view when sign-in link is clicked', async function(assert) {
+    await visit('/sign-up');
+    await click('.sign-in-link');
+    assert.equal(currentURL(), '/sign-in', 'should redirect to sign-in view');
   });
 });
