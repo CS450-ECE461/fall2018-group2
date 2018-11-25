92ca913123a95b84274b69d930288f4015f50ba9
Zachary Balda <zacharybalda23@gmail.com>
2018-11-25 14:59:32 -0500

    fix tests for routes under nav-enabled and add test for nav-enabled route

--- ember-experiences/tests/unit/routes/nav-enabled/experiences-test.js
+++ ember-experiences/tests/unit/routes/nav-enabled/experiences-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | explore', function(hooks) {
+module('Unit | Route | experiences', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:explore');
+    let route = this.owner.lookup('route:nav-enabled.experiences');
     assert.ok(route);
   });
 });
--- ember-experiences/tests/unit/routes/nav-enabled/explore-test.js
+++ ember-experiences/tests/unit/routes/nav-enabled/explore-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | experiences', function(hooks) {
+module('Unit | Route | explore', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:experiences');
+    let route = this.owner.lookup('route:nav-enabled.explore');
     assert.ok(route);
   });
 });
