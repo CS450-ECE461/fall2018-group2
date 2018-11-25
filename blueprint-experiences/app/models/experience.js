79f61fe600a0c11e00528c87aec6f013ed2d0a4b
Leo <oleoneto@gmail.com>
2018-11-25 16:45:05 -0500

    Implement new models

---
+++ blueprint-experiences/app/models/experience.js
@@ -0,0 +1,18 @@
+const mongodb = require ('@onehilltech/blueprint-mongodb');
+const { Schema } = mongodb;
+const { Types: {ObjectId} } =Schema;
+const User = require('./user');
+const Address = require('./address');
+
+const experienceSchema = new Schema ({
+  host: { type: ObjectId, required: true, ref: 'User' },
+
+  address: { type: String, required: true, ref: 'Address' },
+
+  description: { type: String, required: true, trim: true },
+
+  details: { type: String, required: true, trim: true },
+
+}, { softDelete: true });
+
+module.exports = mongodb.resource('user', experienceSchema, 'experiences');
