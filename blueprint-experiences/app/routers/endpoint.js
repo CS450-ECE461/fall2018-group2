7d0aa77373a59b49a498564cadee9cb6ce06d5a0
Leo <oleoneto@gmail.com>
2018-11-27 16:59:24 -0500

    Fixed login

--- blueprint-experiences/app/routers/endpoint.js
+++ blueprint-experiences/app/routers/endpoint.js
@@ -1,16 +1,23 @@
 const blueprint = require('@onehilltech/blueprint');
 const { Router } = require ('@onehilltech/blueprint');
+const cors = require('cors');
 
 /**
  * @class endpoint
  */
-module.exports = Router.extend ({
-  specification: {
-    '/gatekeeper': blueprint.mount ('@onehilltech/blueprint-gatekeeper:v1'),
 
-    // protecting the /v1 routes
-    '/v1': {
-      policy: 'gatekeeper.auth.bearer'
-    }
-  }
-});
+function origin(origin, callback) {
+  console.log('Logged in...');
+  return callback(null, true);
+}
+
+const corsOptions = {
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  origin
+};
+
+
+module.exports = exports  = {
+  '/': {use: [cors(corsOptions)]},
+  '/gatekeeper': blueprint.mount ('@onehilltech/blueprint-gatekeeper:v1'),
+};
