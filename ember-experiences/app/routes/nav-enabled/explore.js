cf91dc19dc9adf18c547c4621302ac1b74f7393a
Zachary Balda <zacharybalda23@gmail.com>
2018-11-29 17:59:15 -0500

    add data to explore route

--- ember-experiences/app/routes/nav-enabled/explore.js
+++ ember-experiences/app/routes/nav-enabled/explore.js
@@ -3,6 +3,27 @@ import Authenticated from 'ember-cli-gatekeeper/mixins/authenticated';
 
 export default Route.extend(Authenticated, {
   model() {
-    return this.store.findAll('experience');
+    return [{
+      description: 'Experience 1 description',
+      details: 'Experience 1 details',
+      address: 'Experience 1 address',
+      host: 'Experience 1 host',
+    }, {
+      description: 'Experience 2 description',
+      details: 'Experience 2 details',
+      address: 'Experience 2 address',
+      host: 'Experience 2 host',
+    }, {
+      description: 'Experience 3 description',
+      details: 'Experience 3 details',
+      address: 'Experience 3 address',
+      host: 'Experience 3 host',
+    }, {
+      description: 'Experience 4 description',
+      details: 'Experience 4 details',
+      address: 'Experience 4 address',
+      host: 'Experience 4 host',
+    }];
+    //return this.store.findAll('experience');
   }
 });
