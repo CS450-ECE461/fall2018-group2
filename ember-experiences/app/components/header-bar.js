c9581a2a072c990615866e2c15adaa2d3a4da5b8
Seth Fuller <semfulle@iupui.edu>
2018-11-29 22:27:35 -0500

    Building more of /experience, replaced all icons with Leos, added theme.scss file for global theming, and refactored theming

--- ember-experiences/app/components/header-bar.js
+++ ember-experiences/app/components/header-bar.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  router: service(),
+
+  showLike: false,
+  showExit: false,
+
+  init() {
+    this._super(...arguments);
+    
+    if (this.get('rightIconType') === "like") this.showLike = true;
+    if (this.get('rightIconType') === "exit") this.showExit = true;
+  },
 });
