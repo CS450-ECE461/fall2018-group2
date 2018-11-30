587ba34e53991757d4a0bf6e3e334d3ee3a34cd9
Zachary Balda <zacharybalda23@gmail.com>
2018-11-30 17:50:47 -0500

    add data to explore page and data bind

--- ember-experiences/app/routes/nav-enabled/explore.js
+++ ember-experiences/app/routes/nav-enabled/explore.js
@@ -4,26 +4,42 @@ import Authenticated from 'ember-cli-gatekeeper/mixins/authenticated';
 export default Route.extend(Authenticated, {
   model() {
     return [{
+      host: {
+        firstName: 'FirstName1',
+        lastName: 'LastName1',
+        roles: 'roles1'
+      },
+      address: {
+        street: 'Experience 1 Street',
+        city: 'City1',
+        state: 'State1',
+        postalCode: 'PostalCode1',
+        country: 'Country1'
+      },
+      title: 'Experience 1 Title',
       description: 'Experience 1 description',
-      details: 'Experience 1 details',
-      address: 'Experience 1 address',
-      host: 'Experience 1 host',
+      price: 100,
+      start: 1,
+      end: 0
     }, {
+      host: {
+        firstName: 'FirstName2',
+        lastName: 'LastName2',
+        roles: 'roles2'
+      },
+      address: {
+        street: 'Experience 2 Street',
+        city: 'City2',
+        state: 'State2',
+        postalCode: 'PostalCode2',
+        country: 'Country2'
+      },
+      title: 'Experience 2 Title',
       description: 'Experience 2 description',
-      details: 'Experience 2 details',
-      address: 'Experience 2 address',
-      host: 'Experience 2 host',
-    }, {
-      description: 'Experience 3 description',
-      details: 'Experience 3 details',
-      address: 'Experience 3 address',
-      host: 'Experience 3 host',
-    }, {
-      description: 'Experience 4 description',
-      details: 'Experience 4 details',
-      address: 'Experience 4 address',
-      host: 'Experience 4 host',
-    }];
+      price: 200,
+      start: 2,
+      end: 20
+    }]
     //return this.store.findAll('experience');
   }
 });
