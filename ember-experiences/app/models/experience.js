f444644518343ac958b7fbd264e5f51ad4e81e73
Zachary Balda <zacharybalda23@gmail.com>
2018-12-03 12:45:50 -0500

    move relations to bottom of model for experience

--- ember-experiences/app/models/experience.js
+++ ember-experiences/app/models/experience.js
@@ -1,11 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  host: DS.belongsTo('user'),
   address: DS.attr('address'),
   title: DS.attr('string'),
   description: DS.attr('string'),
   price: DS.attr('number'),
   start: DS.attr('number'),
-  end: DS.attr('number')
+  end: DS.attr('number'),
+  host: DS.belongsTo('user')
 });
