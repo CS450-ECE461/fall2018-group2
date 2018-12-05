7b9b3e35d2af943e05f204303cfced2be10a840f
Zachary Balda <zacharybalda23@gmail.com>
2018-12-05 14:54:46 -0500

    integrate individual experience with data from backend

--- ember-experiences/app/routes/experience/index.js
+++ ember-experiences/app/routes/experience/index.js
@@ -2,29 +2,7 @@ import Route from '@ember/routing/route';
 import Authenticated from 'ember-cli-gatekeeper/mixins/authenticated';
 
 export default Route.extend(Authenticated, {
-  model() {
-    return {
-      tag: 'default-experience',
-      host: {
-        firstName: 'FirstName1',
-        lastName: 'LastName1',
-        roles: 'roles1'
-      },
-      address: {
-        street: '13498 Rainbow Road',
-        city: 'Indianapolis',
-        state: 'IN',
-        postalCode: '46204',
-        country: 'Country'
-      },
-      title: 'Experience Title',
-      description: 'Experience 1 description',
-      price: 100,
-      start: 1,
-      end: 0
-    }
-    // How to get a specific experience model based on the url:
-    // return this.store.findRecord('rental', params.id);
-    //   also change model() to model(params) at the top
+  model(params) {
+    return this.modelFor('experience');
   }
 });
