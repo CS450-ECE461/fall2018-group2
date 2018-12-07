a340b9063b7625eee743b7bd0ee57832e802eff1
Leo <oleoneto@gmail.com>
2018-12-07 00:27:03 -0500

    Add filter support to bookings and favorites

--- blueprint-experiences/app/controllers/booking.js
+++ blueprint-experiences/app/controllers/booking.js
@@ -1,6 +1,29 @@
 const { ResourceController } = require ('@onehilltech/blueprint-mongodb');
 const { model } = require ('@onehilltech/blueprint');
+const { isEmpty } = require ('lodash');
 
 module.exports = ResourceController.extend ({
-  Model: model ('booking')
+  namespace: 'experiences',
+  Model: model ('booking'),
+
+  getAll () {
+    return this._super.call (this, ...arguments).extend ({
+      schema: {
+        user: {
+          in: 'query',
+          isMongoId: true,
+          isLength: {
+            options: { min: 1 }
+          },
+
+          toMongoId: true,
+          optional: true
+        }
+      },
+
+      getFilter(req, filter){
+        return filter;
+      }
+    })
+  }
 });
