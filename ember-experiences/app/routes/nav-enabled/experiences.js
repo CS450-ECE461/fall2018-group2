0d35b8a53972382cceef6e5a7f3dc7347765e6cb
Leo <oleoneto@gmail.com>
2018-12-07 01:48:49 -0500

    Update experiences route

--- ember-experiences/app/routes/nav-enabled/experiences.js
+++ ember-experiences/app/routes/nav-enabled/experiences.js
@@ -4,13 +4,13 @@ import { hash } from 'rsvp';
 
 export default Route.extend(Authenticated, {
   model() {
+
+    // Gatekeeper user account id
     let currentUser = this.get('currentUser');
 
     return hash({
-      // bookings: this.store.query('booking', { user: currentUser.id }),
-      // favorites: this.store.query('favorite', { user: currentUser.id })
-      bookings: this.store.findAll('booking'),
-      favorites: this.store.findAll('favorite')
+      bookings: this.store.query('booking', { user: currentUser.id }),
+      favorites: this.store.query('favorite', { user: currentUser.id })
     });
   }
 });
