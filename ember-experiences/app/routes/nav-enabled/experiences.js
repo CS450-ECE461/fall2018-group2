15b5dd9e40970329ca42049f20012c3299881cb0
Zachary Balda <zacharybalda23@gmail.com>
2018-12-06 23:31:04 -0500

    populate My Experiences and Favorites with all experiences

--- ember-experiences/app/routes/nav-enabled/experiences.js
+++ ember-experiences/app/routes/nav-enabled/experiences.js
@@ -1,8 +1,16 @@
 import Route from '@ember/routing/route';
 import Authenticated from 'ember-cli-gatekeeper/mixins/authenticated';
+import { hash } from 'rsvp';
 
 export default Route.extend(Authenticated, {
   model() {
-    return this.store.findAll('experience')
+    let currentUser = this.get('currentUser');
+
+    return hash({
+      // bookings: this.store.query('booking', { user: currentUser.id }),
+      // favorites: this.store.query('favorite', { user: currentUser.id })
+      bookings: this.store.findAll('booking'),
+      favorites: this.store.findAll('favorite')
+    });
   }
 });
